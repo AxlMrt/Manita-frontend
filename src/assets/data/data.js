852eec764e53553174db8161f98ada07babd3d09
Axel Martin <amartin@d-edge.com>
2026-01-07 18:08:44 +0100

    Remove Nero Gorgo pizza

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,262 +1,247 @@
 export const pizzaData = [
   {
-    "name": "🇮🇹 La Margherita",
-    "price": 10,
-    "Ingredients": [
-      "Base tomate maison",
-      "mozzarella",
-      "parmesan",
-      "huile d'olive"
-    ],
-    "vegetarian": true,
-    "spicy": false,
-    "monthPizza": false
-  },
-  {
-    "name": "🇮🇹 La Burratina",
-    "price": 12,
-    "Ingredients": [
+    name: "🇮🇹 La Margherita",
+    price: 10,
+    Ingredients: [
       "Base tomate maison",
       "mozzarella",
       "parmesan",
       "huile d'olive",
-      "burratina"
     ],
-    "vegetarian": true,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: true,
+    spicy: false,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La Regina",
-    "price": 13.5,
-    "Ingredients": [
+    name: "🇮🇹 La Burratina",
+    price: 12,
+    Ingredients: [
+      "Base tomate maison",
+      "mozzarella",
+      "parmesan",
+      "huile d'olive",
+      "burratina",
+    ],
+    vegetarian: true,
+    spicy: false,
+    monthPizza: false,
+  },
+  {
+    name: "🇮🇹 La Regina",
+    price: 13.5,
+    Ingredients: [
       "Base tomate maison",
       "mozzarella",
       "jambon cuit aux herbes",
-      "champignons cuisinés maison"
+      "champignons cuisinés maison",
     ],
-    "vegetarian": false,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: false,
+    spicy: false,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La Spianata 🌶️",
-    "price": 13.5,
-    "Ingredients": [
+    name: "🇮🇹 La Spianata 🌶️",
+    price: 13.5,
+    Ingredients: [
       "Base tomate maison",
       "mozzarella",
       "spianata picante",
-      "oignons confits maison"
+      "oignons confits maison",
     ],
-    "vegetarian": false,
-    "spicy": true,
-    "monthPizza": false
+    vegetarian: false,
+    spicy: true,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La Manita 🌶️",
-    "price": 15,
-    "Ingredients": [
+    name: "🇮🇹 La Manita 🌶️",
+    price: 15,
+    Ingredients: [
       "Base tomate maison",
       "mozzarella",
       "jambon cuit aux herbes",
       "spianata picante",
-      "gorgonzola aop"
+      "gorgonzola aop",
     ],
-    "vegetarian": false,
-    "spicy": true,
-    "monthPizza": false
+    vegetarian: false,
+    spicy: true,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La Fresca",
-    "price": 14,
-    "Ingredients": [
+    name: "🇮🇹 La Fresca",
+    price: 14,
+    Ingredients: [
       "Base tomate maison",
       "mozzarella",
       "mortadella di bologna",
       "tomates confites maison",
-      "roquette"
+      "roquette",
     ],
-    "vegetarian": false,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: false,
+    spicy: false,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La 4 Formaggi",
-    "price": 15,
-    "Ingredients": [
+    name: "🇮🇹 La 4 Formaggi",
+    price: 15,
+    Ingredients: [
       "Base crème fraiche assaisonnée",
       "mozzarella",
       "provolone",
       "gorgonzola aop",
-      "parmesan"
+      "parmesan",
     ],
-    "vegetarian": true,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: true,
+    spicy: false,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 L'alpinista",
-    "price": 16,
-    "Ingredients": [
+    name: "🇮🇹 L'alpinista",
+    price: 16,
+    Ingredients: [
       "Base crème fraiche assaisonnée",
       "mozzarella",
       "jambon aux herbes",
       "tallegio aop",
-      "oignons confits maison"
+      "oignons confits maison",
     ],
-    "vegetarian": false,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: false,
+    spicy: false,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La Bianca Rustica",
-    "price": 14,
-    "Ingredients": [
+    name: "🇮🇹 La Bianca Rustica",
+    price: 14,
+    Ingredients: [
       "Base crème fraiche assaisonnée",
       "mozzarella",
       "jambon cuit aux herbes",
       "parmesan",
-      "poivre"
+      "poivre",
     ],
-    "vegetarian": false,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: false,
+    spicy: false,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La Melanzane",
-    "price": 15,
-    "Ingredients": [
+    name: "🇮🇹 La Melanzane",
+    price: 15,
+    Ingredients: [
       "Base tomate maison",
       "mozzarella",
       "provolone",
       "aubergines grillés",
-      "jambon de parme aop"
+      "jambon de parme aop",
     ],
-    "vegetarian": false,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: false,
+    spicy: false,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La Tonno",
-    "price": 15,
-    "Ingredients": [
+    name: "🇮🇹 La Tonno",
+    price: 15,
+    Ingredients: [
       "Base tomate maison",
       "mozzarella",
       "thon",
       "tomates confites maison",
       "oignons crus",
       "olives noires",
-      "huile d'olive"
+      "huile d'olive",
     ],
-    "vegetarian": false,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: false,
+    spicy: false,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La Parma",
-    "price": 15,
-    "Ingredients": [
+    name: "🇮🇹 La Parma",
+    price: 15,
+    Ingredients: [
       "Base tomate maison",
       "mozzarella",
       "provolone",
       "jambon de parme aop",
       "confiture de figues",
-      "amandes grillées"
+      "amandes grillées",
     ],
-    "vegetarian": false,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: false,
+    spicy: false,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La Mortazza",
-    "price": 17,
-    "Ingredients": [
+    name: "🇮🇹 La Mortazza",
+    price: 17,
+    Ingredients: [
       "Base crème de pesto maison",
       "mozzarella",
       "mortadella di bologna",
       "burratina",
-      "pistaches grillées"
+      "pistaches grillées",
     ],
-    "vegetarian": false,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: false,
+    spicy: false,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La Nonna",
-    "price": 17,
-    "Ingredients": [
+    name: "🇮🇹 La Nonna",
+    price: 17,
+    Ingredients: [
       "Base tomate maison",
       "mozzarella",
       "jambon de parme aop",
       "tomates confites maison",
       "burratina",
-      "roquette"
+      "roquette",
     ],
-    "vegetarian": false,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: false,
+    spicy: false,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La Vegetariano",
-    "price": 14,
-    "Ingredients": [
+    name: "🇮🇹 La Vegetariano",
+    price: 14,
+    Ingredients: [
       "Base tomate maison",
       "mozzarella",
       "aubergines grillées",
       "champignons cuisinés maison",
       "tomates confites maison",
-      "burratina"
+      "burratina",
     ],
-    "vegetarian": true,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: true,
+    spicy: false,
+    monthPizza: false,
   },
   {
-    "name": "🇮🇹 La Bella Verde",
-    "price": 14,
-    "Ingredients": [
+    name: "🇮🇹 La Bella Verde",
+    price: 14,
+    Ingredients: [
       "Base tomate maison",
       "mozzarella",
       "aubergines grillées",
       "parmesan",
       "roquette",
-      "huile d'olive"
+      "huile d'olive",
     ],
-    "vegetarian": true,
-    "spicy": false,
-    "monthPizza": false
+    vegetarian: true,
+    spicy: false,
+    monthPizza: false,
   },
-  {
-    "name": "La Nero Gorgo",
-    "price": 18,
-    "Ingredients": [
-      "Base crème d'ail noir",
-      "mozzarella",
-      "pancetta",
-      "champignons cuisinés",
-      "gorgonzola",
-      "amandes"
-    ],
-    "vegetarian": false,
-    "spicy": false,
-    "monthPizza": false
-  }
 ];
 
 export const panuozziData = [];
 
 export const dessertData = [
-  { "name": "Panna Cotta Myrtille", "price": 4 },
-  { "name": "Panna Cotta Fruits Rouges", "price": 4 },
-  { "name": "Panna Cotta Caramel Beurre Salé", "price": 4 }
+  { name: "Panna Cotta Myrtille", price: 4 },
+  { name: "Panna Cotta Fruits Rouges", price: 4 },
+  { name: "Panna Cotta Caramel Beurre Salé", price: 4 },
 ];
 
 export const drinkData = [
-  { "name": "Coca Cola 33cl", "price": 2 },
-  { "name": "Coca Cola Zéro 33cl", "price": 2 },
-  { "name": "Coca Cola Cherry 33cl", "price": 2 },
-  { "name": "Fuze Tea Pêche 33cl", "price": 2 },
-  { "name": "Oasis Tropical 33cl", "price": 2 },
-  { "name": "Fanta Orange 33cl", "price": 2 },
-  { "name": "Eau Minérale 50cl", "price": 1.5 }
+  { name: "Coca Cola 33cl", price: 2 },
+  { name: "Coca Cola Zéro 33cl", price: 2 },
+  { name: "Coca Cola Cherry 33cl", price: 2 },
+  { name: "Fuze Tea Pêche 33cl", price: 2 },
+  { name: "Oasis Tropical 33cl", price: 2 },
+  { name: "Fanta Orange 33cl", price: 2 },
+  { name: "Eau Minérale 50cl", price: 1.5 },
 ];
